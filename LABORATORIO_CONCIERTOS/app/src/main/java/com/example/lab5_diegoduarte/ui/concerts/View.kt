8d7da408b9d67c826aee3aee912c9fda187c7be7
DiegoDuaS <dua22075@uvg.edu.gt>
2023-09-07 08:05:00 -0600

    Estructura

---
+++ LABORATORIO_CONCIERTOS/app/src/main/java/com/example/lab5_diegoduarte/ui/concerts/View.kt
@@ -0,0 +1,176 @@
+package com.example.lab5_diegoduarte.ui.concerts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lab5_diegoduarte.Conciertos
+import com.example.lab5_diegoduarte.R
+import com.example.lab5_diegoduarte.ui.theme.BlueCito
+import com.example.lab5_diegoduarte.ui.theme.BlueOscuro
+import com.example.lab5_diegoduarte.ui.theme.Fondo
+import com.example.lab5_diegoduarte.ui.theme.Fondo1
+import com.example.lab5_diegoduarte.ui.theme.LAB5_DiegoDuarteTheme
+import com.example.lab5_diegoduarte.ui.theme.Tarjetitas
+
+@Composable
+fun Pantalla1(){
+    val ConciertosFav = remember { mutableStateListOf<Conciertos>() }
+    val ConciertosAll = remember { mutableStateListOf<Conciertos>() }
+    ConciertosFav.add(Conciertos("Young Miko", "9 de Septiembre, 2023", R.drawable.youngmiko))
+    ConciertosFav.add(Conciertos("Latin Mafia", "20 de Octube, 2023", R.drawable.latinmafia))
+    ConciertosFav.add(Conciertos("Twice", "2 de Noviembre, 2023", R.drawable.twice))
+    ConciertosFav.add(Conciertos("Feid", "4 de Diciembre, 2023", R.drawable.ferxxo))
+    ConciertosAll.add(Conciertos("Young Miko", "9 de Septiembre, 2023", R.drawable.youngmiko))
+    ConciertosAll.add(Conciertos("Latin Mafia", "20 de Octube, 2023", R.drawable.latinmafia))
+    ConciertosAll.add(Conciertos("Twice", "2 de Noviembre, 2023", R.drawable.twice))
+    ConciertosAll.add(Conciertos("Feid", "4 de Diciembre, 2023", R.drawable.ferxxo))
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Fondo),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Box(
+            modifier = Modifier
+                .background(color = Fondo1)
+                .fillMaxWidth()
+        ){
+            Text(
+                text = "Todo Eventos",
+                style = androidx.compose.material.MaterialTheme.typography.h4,
+                modifier = Modifier
+                    .padding(bottom = 8.dp, top = 8.dp)
+                    .align(Alignment.Center),
+                color = Color.White
+            )
+        }
+        Text(
+            text = "Eventos Favoritos",
+            style = androidx.compose.material.MaterialTheme.typography.h6,
+            modifier = Modifier
+                .padding(bottom = 8.dp, top = 8.dp),
+            color = BlueOscuro,
+            textAlign = TextAlign.Start,
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+        ){
+            ConciertosFav.forEach { concierto ->
+                CardConcierto(concierto = concierto)
+            }
+        }
+        Text(
+            text = "Todos Los Conciertos",
+            style = androidx.compose.material.MaterialTheme.typography.h6,
+            modifier = Modifier.padding(bottom = 8.dp, top = 8.dp),
+            color = BlueOscuro
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+        ){
+            ConciertosAll.forEach { concierto ->
+                CardConcierto(concierto = concierto)
+            }
+        }
+    }
+}
+
+@Composable
+fun CardConcierto(concierto: Conciertos){
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .background(color = Fondo)
+            .width(150.dp)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Tarjetitas),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .background(color = Tarjetitas)
+            ){
+                Image(
+                    painter = painterResource(id = concierto.image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(170.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text(
+                text = concierto.name,
+                style = androidx.compose.material.MaterialTheme.typography.h6,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth(),
+                color = BlueOscuro,
+                textAlign = TextAlign.Center
+
+            )
+            Text(
+                text = concierto.date,
+                style = androidx.compose.material.MaterialTheme.typography.body2,
+                modifier = Modifier
+                    .padding(vertical = 2.dp)
+                    .fillMaxWidth(),
+                color = BlueOscuro,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
